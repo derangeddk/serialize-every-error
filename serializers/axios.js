2bf93c8a2316456d7e198046c18f225b64fa4bd4
Niels Roesen Abildgaard <niels.abildgaard@gmail.com>
2022-10-11 12:13:39 +0200

    Set up axios serializer properly

--- serializers/axios.js
+++ serializers/axios.js
@@ -1,5 +1,49 @@
+import pick from 'lodash.pick';
+
 export default (err, serializeError) => {
-  
+  const serialized = pick(err, [
+    "code",
+    "isAxiosError",
+  ]);
+
+  if(err.config) {
+    serialized.config = pick(err.config, [
+        "xsrfCookieName",
+        "xsrfHeaderName",
+        "timeout",
+        "maxContentLength",
+        "maxBodyLength",
+        "headers",
+        "baseURL",
+        "method",
+        "url",
+        "data",
+    ]);
+  }
+
+  if(err.request) {
+    serialized.request = pick(err.request, [
+        "finished",
+        "method",
+        "path",
+        "aborted",
+        "host",
+        "protocol",
+        "_hasBody",
+        "_headerSent",
+    ]);
+  }
+
+  if(err.response) {
+    serialized.response = pick(err.response, [
+        "status",
+        "statusText",
+        "headers",
+        "data",
+    ]);
+  }
+
+  return serialized;
 };
 
 export const canHandle = (err) => err.isAxiosError;
